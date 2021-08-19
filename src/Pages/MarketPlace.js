68b71d47946a1d782e0596ff3e3b2284236097af
Soo-Beom <soobeomkim77@gmail.com>
2021-08-18 17:17:45 -0700

    switching app.js to functional, recreating top down structure

--- src/Pages/MarketPlace.js
+++ src/Pages/MarketPlace.js
@@ -7,87 +7,43 @@ import { useState } from 'react'
 
 const Market = () => {
   const [items, setItems] = useState();
+  const [ categoryFilterOption, setCatergoryFilterOption ] = useState('all')
+  const [userFilterOption, setUserFilterOption] = useState('')
 
-  // const handleCategoryDropDown = (event, data) => {
-  //   this.setState({
-  //     categoryFilterOption: data.value
-  //   })
-  // }
 
-  //  const handleCategoryDropDown = (event, data) => {
-  //   this.setState({
-  //     categoryFilterOption: data.value
-  //   })
-  // }
+  const getItems = () => {
+    let configObj = {
+      method: "GET",
+      headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    };
 
-  //  const handleUserFilter = (e) => {
-  //   this.setState({
-  //     userFilterOption: e.target.value
-  //   })
-  // }
-
-  // const filterItems = () => {
-  //   /* Category is not All & User search is blank */
-  //   if (this.state.categoryFilterOption ===! 'all' && !this.state.userFilterOption){
-  //     return this.props.items.filter(item => item.category === this.state.categoryFilterOption)
-    
-  //     /* Category = All & User search is blank */
-  //   } else if (this.state.categoryFilterOption === 'all' && !this.state.userFilterOption){
-  //     return this.props.items 
-    
-  //     /* Category = All & User search is not blank */
-  //   } else if (this.state.categoryFilterOption === 'all' && this.state.userFilterOption){
-  //     return this.props.items.filter(item => item.name.toLowerCase().includes(this.state.userFilterOption.toLowerCase()))
-    
-  //     /* Category is not blank & User search is not blank */
-  //   } else {
-  //     return this.props.items.filter(item => item.category === this.state.categoryFilterOption && item.name.toLowerCase().includes(this.state.userFilterOption.toLowerCase()))
-  //   }
-  // }
-
-  return (
-    <div>
-
-    </div>
-  )
-}
-
-class MarketPlace extends React.Component {
-
-  state={
-    categoryFilterOption: 'all',
-    userFilterOption: ''
+    fetch('http://localhost:3000/items', configObj)
+    .then(response => response.json())
+    .then(data => setItems(data))
+  
   }
-
-
-//   renderContent = () => {
-//    if (this.props.itemView !== false) {
-//         return <ItemSpecs item={this.props.item} goBack={this.props.goBack} editItem={this.props.editItem} />; 
-//     } else {
-//         return <ItemContainer myPage={this.state.myPage} remove={this.props.remove} items={this.props.items} view={this.props.view} />;
-//     }
-// }
-
-
-  handleCategoryDropDown = (event, data) => {
+  const handleCategoryDropDown = (event, data) => {
     this.setState({
       categoryFilterOption: data.value
     })
   }
 
-  handleCategoryDropDown = (event, data) => {
+   const handleCategoryDropDown = (event, data) => {
     this.setState({
       categoryFilterOption: data.value
     })
   }
 
-  handleUserFilter = (e) => {
+   const handleUserFilter = (e) => {
     this.setState({
       userFilterOption: e.target.value
     })
   }
 
-  filterItems = () => {
+  const filterItems = () => {
     /* Category is not All & User search is blank */
     if (this.state.categoryFilterOption ===! 'all' && !this.state.userFilterOption){
       return this.props.items.filter(item => item.category === this.state.categoryFilterOption)
@@ -106,25 +62,86 @@ class MarketPlace extends React.Component {
     }
   }
 
-  render() {
-    return(
-      <div>
-        {!this.props.isLoggedIn ? 
-        null 
-        :
-        <Grid columns={2} divided stackable >
-          <Grid.Column width="3" >
-            <FilterBar handleCategoryDropDown={this.handleCategoryDropDown} handleUserFilter={this.handleUserFilter} currentUser={this.props.currentUser} />
-          </Grid.Column>
-          <Grid.Column width="12">
-            {!this.props.itemView ? <ItemContainer items={this.filterItems()} view={this.props.view} item={this.props.item} /> : <ItemSpecs item={this.props.item} goBack={this.props.goBack} buy={this.props.buy} />}
-          </Grid.Column>
-        </Grid> 
+  return (
+    <div>
+        Items go Here
+    </div>
+  )
+}
+
+// class MarketPlace extends React.Component {
+
+//   state={
+//     categoryFilterOption: 'all',
+//     userFilterOption: ''
+//   }
+
+
+// //   renderContent = () => {
+// //    if (this.props.itemView !== false) {
+// //         return <ItemSpecs item={this.props.item} goBack={this.props.goBack} editItem={this.props.editItem} />; 
+// //     } else {
+// //         return <ItemContainer myPage={this.state.myPage} remove={this.props.remove} items={this.props.items} view={this.props.view} />;
+// //     }
+// // }
+
+
+//   handleCategoryDropDown = (event, data) => {
+//     this.setState({
+//       categoryFilterOption: data.value
+//     })
+//   }
+
+//   handleCategoryDropDown = (event, data) => {
+//     this.setState({
+//       categoryFilterOption: data.value
+//     })
+//   }
+
+//   handleUserFilter = (e) => {
+//     this.setState({
+//       userFilterOption: e.target.value
+//     })
+//   }
+
+//   filterItems = () => {
+//     /* Category is not All & User search is blank */
+//     if (this.state.categoryFilterOption ===! 'all' && !this.state.userFilterOption){
+//       return this.props.items.filter(item => item.category === this.state.categoryFilterOption)
+    
+//       /* Category = All & User search is blank */
+//     } else if (this.state.categoryFilterOption === 'all' && !this.state.userFilterOption){
+//       return this.props.items 
+    
+//       /* Category = All & User search is not blank */
+//     } else if (this.state.categoryFilterOption === 'all' && this.state.userFilterOption){
+//       return this.props.items.filter(item => item.name.toLowerCase().includes(this.state.userFilterOption.toLowerCase()))
+    
+//       /* Category is not blank & User search is not blank */
+//     } else {
+//       return this.props.items.filter(item => item.category === this.state.categoryFilterOption && item.name.toLowerCase().includes(this.state.userFilterOption.toLowerCase()))
+//     }
+//   }
+
+//   render() {
+//     return(
+//       <div>
+//         {!this.props.isLoggedIn ? 
+//         null 
+//         :
+//         <Grid columns={2} divided stackable >
+//           <Grid.Column width="3" >
+//             <FilterBar handleCategoryDropDown={this.handleCategoryDropDown} handleUserFilter={this.handleUserFilter} currentUser={this.props.currentUser} />
+//           </Grid.Column>
+//           <Grid.Column width="12">
+//             {!this.props.itemView ? <ItemContainer items={this.filterItems()} view={this.props.view} item={this.props.item} /> : <ItemSpecs item={this.props.item} goBack={this.props.goBack} buy={this.props.buy} />}
+//           </Grid.Column>
+//         </Grid> 
          
-        }
-      </div>
-    )
-    }
-  }
+//         }
+//       </div>
+//     )
+//     }
+//   }
 
 export default MarketPlace 
